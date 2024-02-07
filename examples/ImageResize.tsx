a4c93b6f94e79ef33fa36d223b3ff952fe305abb
huangwenping <huangwenping@didiglobal.com>
2024-02-07 16:28:24 +0800

    chore: test examples

--- examples/ImageResize.tsx
+++ examples/ImageResize.tsx
@@ -39,20 +39,32 @@ const defaultText = `
 **bold**
 * Markdown 作为轻量级标记语言被广泛使用，多数产研同学熟悉Markdown语法，是撰写技术文档的首选方案；2
 * 编辑器在文本格式编辑上提供了交互友好的可视化工具，如图片上传、链接、代码、表格等； 
+__aa__
+==bb==
+
+![](https://s3-gz01.didistatic.com/base-docs/base-docs/20220711205536478cf99orw5ly.png "right-50,width=560")
+
 ## heading2
 `;
+
+const html = renderToHtml(defaultText);
+console.log(html);
 const Editor = (): ReactElement => {
   const editorRef = useRef<RMEditor>(null);
 
   const onChange = (callback: () => string) => {
     console.log(callback());
     if (editorRef.current) {
-      console.log("------------", editorRef.current.view.state.tr);
+      // console.log(editorRef.current.view.state.doc.content.forEach(item => ));
     }
   };
 
   return (
     <div style={{ margin: "50px 100px" }}>
+      <div
+        dangerouslySetInnerHTML={{ __html: html }}
+        className="render-html"
+      ></div>
       <h2>Basic Example</h2>
       <div
         style={{
